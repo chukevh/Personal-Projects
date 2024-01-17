00a1e469f6d20b5e4f1c9296868144d7aba55628
Kevin Chu <chu.kevin.h@gmail.com>
2024-01-17 15:35:41 +1100

    Learning React

--- react/index.js
+++ react/index.js
@@ -1,23 +1,49 @@
-// import React from "react"
-// import ReactDOM from "react-dom"
+// React component - returns react elements, (UI), resuable code
 
 function Page() {
     return (
         <div>
-            <header>
-                <nav>
-                    <img src="./github.png" width="40px"/>
-                </nav>
-            </header>
-            <h1>Why I'm excited to learn React</h1>
-            <ul>
-                <li>It's sick</li>
-                <li>Frontend is easy af</li>
-            </ul>
-            <footer>
-                <small>Copyright 2023 Kevin Chu Development. All rights reserved.</small>
-            </footer>
+            <Header />
+            <MainContent />
+            <Footer />
         </div>
+    )
+}
+
+function Header() {
+    return (
+        <header>
+            <nav className="nav">
+                <img src="./github.png" width="40px"/>
+                <ul className="nav-items">
+                    
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+function MainContent() {
+    return (
+        <div>
+            <h1>Why I'm excited to learn React</h1>
+            <ol>
+                <li>React is Cool</li>
+                <li>I like Frontend</li>
+                <li>Erica is cute</li>
+            </ol>
+        </div>
+    )
+}
+
+function Footer()  {
+    return (
+        <footer>
+            <small>Copyright 2023 Kevin Chu Development. All rights reserved.</small>
+        </footer>
     )
 }
 
